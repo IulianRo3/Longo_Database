c08640aa7569db6b2a787571ec7c17bac1868962
Dumbravă Iulian <61887287+IulianRo3@users.noreply.github.com>
2021-08-15 23:08:18 +0300

    Update Longo_Exercises.sql

--- Longo_Exercises.sql
+++ Longo_Exercises.sql
@@ -11,6 +11,7 @@ where id_angajat = 2;
 
 /*Vom mai folosii UPDATE-ul pentru marirea salariului angajatilor care-l au mai mic de 3000 de euro cu 500 si scaderea celor care-l au mai mare de 10000 
 We will also use the UPDATE to increase the salary of employees who have less than 3,000 euros by 500 and decrease those who have more than 10,000*/
+
 Update AngajatiFirma                                                 
 set Salariu = Salariu + 500                                             
 where Salariu < 3000;                                                    
@@ -21,11 +22,13 @@ where Salariu > 10000;
 
 /*Vom sterge comenzile din 2018 deoarece nu mai sunt de folos 
 We will delete the orders from 2018 because they are no longer useful*/
+
 delete from Comenzi_Clienti                             
 where Data_Comanda < '01.JAN.2019'; 
 
 /*Vom fi nevoiti sa dam afara un angajat si in acelasi timp dupa concedierea acestuia sa vindem camionul respectiv 
 We will have to fire an employee and at the same time after his dismissal we will sell the respective truck*/
+
 delete from Camioane
 where cod_camion = 1; 
 
@@ -34,14 +37,17 @@ where id_angajat = 1;
 
 /*Am sters din greseala camionul incorect 
 I accidentally deleted the wrong truck*/
+
 delete from Camioane where cod_camion = 3;
 
 /*Si am folosit comanda “rollback” pentru a-l “readuce”
 And I used the "rollback" command to "bring it back": */
+
 rollback; 
 
 /*Am sters tabela Comenzi_Clienti incurcand-o cu o tabela cu nume asemanator.Am reusit sa o readuc inapoi prin comanda flashback
 I deleted the Clients_Commands table by confusing it with a table with a similar name. I managed to bring it back with the flashback command */
+
 FLASHBACK TABLE comenzi_clienti           
 TO BEFORE DROP;
 
@@ -56,27 +62,32 @@ values(417,TO_DATE('01.MAY.2020','DD/MM/YYYY'),'Tractor',101,10,10,'Suceava');
 Examples of miscellaneous queries (SELECT)
 
 Inner Join*/
+
 select nr_comanda,incarcatura,a.id_client,nume
 from clienti_firma a inner join comenzi_clienti b
 on a.id_client = b.id_client
 order by id_client ASC;
 
 /* Right Join*/
+
 select cod_camion,a.id_angajat,Nume
 from angajatifirma a right join camioane b
 on a.id_angajat = b.id_angajat;
 
 /* Left Join */
+
 select cod_camion,a.id_angajat,Nume
 from angajatifirma a left join camioane b
 on a.id_angajat = b.id_angajat;
 
 /* Full Outer Join */
+
 select cod_camion,a.id_angajat,Nume
 from angajatifirma a full outer join camioane b
 on a.id_angajat = b.id_angajat;                                                                                                      
 
 /* Self Join */
+
 select a.nume || ' lucreaza pentru ' || m.nume Cine_lucreaza_pentru_cine
 from angajatifirma a,angajatifirma m
 where a.id_manager = m.id_angajat
@@ -84,7 +95,9 @@ order by a.nume;
 
 /* Functii de grup 
 Group Functions
+
  MAX */
+ 
 select MAX(salariu) Cel_mai_mare_salariu,Nume,Prenume,data_angajare
 from angajatifirma
 where data_angajare > '01.jan.2015'
@@ -92,12 +105,14 @@ group by(Nume,Prenume,data_angajare)
 order by data_angajare asc;
 
 /* MIN */
+
 select MIN(data_angajare)Prima_Angajare,MAX(salariu) Cel_mai_mare_salariu,
 MAX(data_angajare)Ultima_Angajare,MIN(salariu) Cel_mai_mic_salariu,COUNT(salariu)
 from angajati_firma
 where id_angajat < 11;
 
 /* CASE */
+
 select id_angajat,nume,prenume,salariu,salariu+(CASE id_angajat
 when 4 then 500
 ELSE 0
@@ -115,10 +130,12 @@ from angajatifirma
 order by salariu asc;
 
 /* DECODE */
+
 select id_angajat,nume,prenume,salariu+(decode(id_angajat,4,500))majorare_salariu
 from angajatifirma;
 
 /* FUNCTII PREDEFINITE */
+
 select id_angajat,nume,prenume,lower(substr(nume,1,2) || substr(prenume,1,2) || substr(id_functie,1,2) ||'@yahoo.ro' )Mail_Firma,
 upper(substr(prenume,1,2)) || lower(substr(nume,1,3)) Parola
 from angajatifirma;
@@ -134,6 +151,7 @@ where nr_comanda = 403;
 
 /* CERERI IERARHICE
 HIERARCHICAL REQUESTS */
+
 select id_angajat,nume,prenume,level,id_manager,connect_by_root(nume)
 from angajatifirma
 connect by prior id_angajat= id_manager
@@ -146,6 +164,7 @@ order by id_angajat ASC;
 
 /* CERERI IMBRICATE(SUBCERERI)
 NESTED QUERIES (SUBQUERY) */
+
 SELECT * 
 FROM angajatifirma 
 WHERE id_angajat IN (SELECT id_angajat 
@@ -159,7 +178,8 @@ FROM camioane
 WHERE kilometraj >432102 ) ;
 
 /* SINONIME
-SYNONYMY synonymy */
+SYNONYMY */
+
 create synonym AF                                                                                                       
 for angajati_firma;
 
@@ -173,6 +193,7 @@ drop synonym AF/CC/CF.
 
 /* TABELE VIRTUALE
 VIRTUAL TABLES */
+
 create view ang as select * from AF
 where id_angajat <11;
 
